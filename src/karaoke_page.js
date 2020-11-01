b9e0e12212ebdcec1082ec03151c601d825bb4b4
rodericasm <rodericasm@gmail.com>
2020-11-01 21:48:11 +1000

    added video.js

--- src/karaoke_page.js
+++ src/karaoke_page.js
@@ -3,13 +3,32 @@ import EASY_VIDEO from "./assets/easy_web.m4v";
 import MIC_CONTROLLER from "./components/mic_controller";
 import "./styles/karaoke.css";
 
+
 export default function Karaoke_page() {
   return (
     <div className="karaoke_container">
       <MIC_CONTROLLER></MIC_CONTROLLER>
-      <video id="karaoke_video" className="karaoke_video" controls>
-        <source src={EASY_VIDEO} type="video/mp4" />
+
+      
+      <video
+          
+          class="video-js"
+          id="karaoke_video"
+          height="100vh"
+          width="100vw"
+          controls
+          preload="auto"
+          data-setup='{}'>
+        <source src={EASY_VIDEO} type="video/mp4"></source>
+        <p class="vjs-no-js">
+          To view this video please enable JavaScript, and consider upgrading to a
+          web browser that
+          <a href="https://videojs.com/html5-video-support/" target="_blank">
+            supports HTML5 video
+          </a>
+        </p>
       </video>
+
     </div>
   );
 }
