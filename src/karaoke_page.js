6b8b1fa96458783b18e492e2bdbdfa55b48f89e1
rodericasm <rodericasm@gmail.com>
2020-11-02 05:31:20 +1000

    fixd sec risk

--- src/karaoke_page.js
+++ src/karaoke_page.js
@@ -23,9 +23,6 @@ export default function Karaoke_page() {
         <p class="vjs-no-js">
           To view this video please enable JavaScript, and consider upgrading to a
           web browser that
-          <a href="https://videojs.com/html5-video-support/" target="_blank">
-            supports HTML5 video
-          </a>
         </p>
       </video>
 
