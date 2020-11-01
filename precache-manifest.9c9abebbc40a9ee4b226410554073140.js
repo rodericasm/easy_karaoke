5f46779558d705c79d820b01c64cb4971478d9e9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-01 19:33:29 +0000

    Deploy rodericasm/easy_karaoke to github.com/rodericasm/easy_karaoke.git:gh-pages

---
+++ precache-manifest.9c9abebbc40a9ee4b226410554073140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b57a3fb185943687ff836a0018dcce9",
+    "url": "/easy_karaoke/index.html"
+  },
+  {
+    "revision": "c7fd5bd338e1a3762fc2",
+    "url": "/easy_karaoke/static/css/main.150ee4be.chunk.css"
+  },
+  {
+    "revision": "1f4389db144c51fd7e5e",
+    "url": "/easy_karaoke/static/js/2.508b387d.chunk.js"
+  },
+  {
+    "revision": "e58b2fed1a83725c5b0108382043ede5",
+    "url": "/easy_karaoke/static/js/2.508b387d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7fd5bd338e1a3762fc2",
+    "url": "/easy_karaoke/static/js/main.4e79d4dc.chunk.js"
+  },
+  {
+    "revision": "6aa11f79a209769a3f28",
+    "url": "/easy_karaoke/static/js/runtime-main.f28274a9.js"
+  },
+  {
+    "revision": "a30e9bcfd763516c3ca52374a231abb8",
+    "url": "/easy_karaoke/static/media/easy_web.a30e9bcf.m4v"
+  }
+]);
